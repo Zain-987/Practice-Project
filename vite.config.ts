493022ebcb31d38308d8b1008417e79204eb0e0b
Zain-987 <zm129991@gmail.com>
2024-01-01 08:21:48 -0800

    Commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-import VitePluginPages from 'vite-plugin-pages';
-import VitePluginGhPages from 'vite-plugin-gh-pages';
-
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: 'Practice-Project',
-  plugins: [VitePluginPages(),
-  VitePluginGhPages(),
+  plugins: [
 react()],
 })
